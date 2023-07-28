0d8821ba96735ee2b64e9cbf717e04a808eba032
StephanyKleen <stephanykleen@gmail.com>
2023-07-28 13:26:46 -0700

    Using the TDD method to create tests before writing our code

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,9 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  if(typeof string !== 'string'){throw new TypeError('argument is not a string');}
+  
+  let newString = '';
+  for(let  i = string.length - 1; i >= 0; i--){
+    newString += string[i]
+  }
+  return newString;
 };
