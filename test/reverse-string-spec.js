67c95b7356b67ce324726c52b15a1166960e827d
StephanyKleen <stephanykleen@gmail.com>
2023-07-28 13:28:17 -0700

    Creating test-specs from scratch with Mocha, Chai to reverse a string and throw an error if argument is not a string type

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,23 @@
-// Your code here
+const chai = require('chai');
+const expect = chai.expect;
+const reverseString = require('../problems/reverse-string')
+
+
+describe('reverseString()', function(){
+    let str;
+    let num;
+
+    this.beforeEach( ()=>{
+        str = 'fun'
+        num = 5;
+    })
+    it('should reverse input string', function(){
+        expect(reverseString(str)).to.equal('nuf');
+        //expect(reverseString('happy')).to.equal('yppah');
+        
+    })
+
+    it('should throw error when input is not a string', function(){
+        expect(() => reverseString(num)).to.throw(TypeError)
+    })
+})
