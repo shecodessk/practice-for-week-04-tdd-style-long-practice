cca0fdbbb4579c231a144e5d3e46b69234918430
StephanyKleen <stephanykleen@gmail.com>
2023-07-31 20:32:09 -0700

    phase 5: bonus points to solve specs for function without map method.

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,17 @@
 function myMap(inputArray, callback) {
-  // Your code here
+  const newArray = [];
+    for(let i = 0; i < inputArray.length; i++){
+      let el = inputArray[i];
+      newArray.push(callback(el))
+    }
+    return newArray;
 }
 
+
+/*const arr = [1, 2, 3];
+const callback = (el) => el * 2;
+
+console.log(myMap(arr, callback)); // prints [2,4,6]
+console.log(arr); // prints [1,2,3]
+*/
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,51 @@
-// Your code here
+const chai = require('chai');
+const expect = chai.expect;
+const spies = require('chai-spies');
+chai.use(spies);
+
+const myMap  = require('../problems/my-map')
+    let cb;
+    let array;
+    let newArr;
+    beforeEach( () =>{
+        cb = (el) => {return el * 2};
+        array = [1,2,3];
+        newArr = myMap(array, cb)
+    })
+
+describe('myMap()', () => {
+
+     it('returns new array where cb was called on each original element', () => {
+        expect(newArr).to.be.eql([2, 4, 6])
+    })
+      it('does not mutate original array', () => {
+        expect(newArr).to.be.eql([2, 4, 6])
+        expect(array).to.be.eql([1, 2, 3])
+    })
+
+        it('should not use the Array.map method', () => {
+        const spy = chai.spy.on(array, 'map');
+        expect(spy).to.have.not.been.called();
+    });
+
+    it('should call the passed-in callback function', () => {
+        const spy = chai.spy(cb);
+        const arr = myMap(array, spy);
+        expect(spy).to.have.been.called();
+    })
+
+    context('if a empty array is provided', () => {
+        it('should return an empty array if a empty array is provided', () => {
+            expect(myMap([], cb)).to.eql([]);
+        });
+    });
+
+    context('if the argument is not an array or the callback is not a function', () => {
+        it('should throw a TypeError', () => {
+            expect(() => myMap(null, cb)).to.throw(TypeError);
+            expect(() => myMap(array, null)).to.throw(TypeError);
+        });
+    });
+   
+  
+})
